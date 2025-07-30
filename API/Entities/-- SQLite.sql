e14f9442a3788b76a57a576576f77a7b9df3276f
jongkeshjuit <23520281@gm.uit.edu.vn>
2025-07-30 14:10:04 +0700

    Day 1 section 2

---
+++ API/Entities/-- SQLite.sql
@@ -0,0 +1,7 @@
+-- SQLite
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ('bob-id', 'Bob', 'bob@test.com');
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ('alice-id', 'Alice', 'alice@test.com');
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ('charlie-id', 'Charlie', 'charlie@test.com');
